c6534e4b988793dfd1e2c62252ba7c0d768e2473
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-18 22:07:33 -0600

    update about

--- src/js/app.js
+++ src/js/app.js
@@ -71,3 +71,55 @@ document.addEventListener(
             });
     }, !1
 );
+
+function calularEdad() {
+    var Meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var FechaString = "05/07/1991";
+
+    var FechaNacimiento = new Date(FechaString);
+    var FechaActual = new Date();
+    var anios = (FechaActual.getFullYear() - FechaNacimiento.getFullYear());
+    var mes = new Date().getMonth();
+    var Es_o_Ya_Paso_CumpleaAnio = EsoYaPasoCumpleAnios(FechaNacimiento, FechaActual);
+
+    var edad = (Es_o_Ya_Paso_CumpleaAnio) ? anios : (--anios);
+
+
+    if (EsCumpleAnios(FechaNacimiento, FechaActual)) {
+        $('#greet').modal('show');
+        $('#BodyHappy').append('Hoy ' + (mes + 1) + ' de ' + Meses[mes] + ' cumple ' + edad + ' de edad');
+        $('#BodyHappy').append('<br><br>Felicidades!! 😇');
+
+    }
+
+    edad = (edad).toString();
+    $("#edad").text(edad + " años");
+}
+
+function EsoYaPasoCumpleAnios(FechaNacimiento, FechaActual) {
+
+    var Es_o_Ya_Paso_CumpleaAnio = false;
+    var YaPasoMesCumpleaños = ((FechaActual.getMonth() + 1) > (FechaNacimiento.getMonth() + 1));
+    var EsMesCumpleaños = ((FechaActual.getMonth() + 1) == (FechaNacimiento.getMonth() + 1));
+    var YaPasoDiaCumpelaños = (FechaActual.getDate() >= FechaNacimiento.getDate());
+
+    if (YaPasoMesCumpleaños) {
+        Es_o_Ya_Paso_CumpleaAnio = true;
+    } else if (EsMesCumpleaños && YaPasoDiaCumpelaños) {
+        Es_o_Ya_Paso_CumpleaAnio = true;
+    }
+    return Es_o_Ya_Paso_CumpleaAnio;
+}
+
+function EsCumpleAnios(FechaNacimiento, FechaActual) {
+
+    var Es_CumpleaAnios = false;
+
+    var EsMesCumpleaños = ((FechaActual.getMonth() + 1) == (FechaNacimiento.getMonth() + 1));
+    var EsDiaCumpelaños = (FechaActual.getDate() === FechaNacimiento.getDate());
+
+    if (EsMesCumpleaños && EsDiaCumpelaños) {
+        Es_CumpleaAnios = true;
+    }
+    return Es_CumpleaAnios;
+}
